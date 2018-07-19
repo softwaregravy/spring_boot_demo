df0959e3ebfed86490e9b9eb2ef97700cacea9ef
John Hinnegan <github-commit@johnhinnegan.com>
2018-07-19 18:19:52 -0400

    basic example

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String Hello() {
+        return "Hello World";
+
+    }
+}
